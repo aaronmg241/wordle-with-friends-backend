71e9dbfc677a7762c75aa6b071eae10dcd127969
aaronmg241 <agrubb@uoguelph.ca>
2023-06-22 15:53:16 -0300

    Adding files for Railway

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,15 +26,15 @@ SECRET_KEY = 'django-insecure-u6l55vtfp8h0yj04*betj^kh2ow%#pwkmqw#6g6*furkrmq)wx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '192.168.2.18'
-]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://192.168.2.18:5173"
 ]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
