44a89829953b659424e5eca148b4795f9ae73048
aaronmg241 <agrubb@uoguelph.ca>
2023-06-23 14:40:46 -0300

    Debugging connecting to redis

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ["REDIS_URL"], os.environ["REDISPORT"])],
+            "hosts": [(os.environ["REDIS_HOST"], os.environ["REDISPORT"])],
         },
     },
 }
